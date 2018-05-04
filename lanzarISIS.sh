6aef1611200a9ac5e3a57eac08a73c66ca485836
Rodriguez Diaz Alejandro <i0917867@sundia04.cie.aulas.usal.es>
2018-05-04 16:59:06 +0200

    new launcher

--- lanzarISIS.sh
+++ lanzarISIS.sh
@@ -7,7 +7,6 @@ function verbose {
     servidor"
 
     maquinas=()
-    puertos=()
     indice=1
     rm -f servidores.conf
     touch servidores.conf
@@ -15,7 +14,6 @@ function verbose {
     while true
     do
         read -p "Introducir la ip de la maquina > " ipMaquina
-        read -p "Introducir el puerto por el que escuchar > " puertoMaquina
 
         if [ -z "$ipMaquina" ]
         then
@@ -23,15 +21,10 @@ function verbose {
         elif [ -z "$ipMaquina" ]
         then
             echo "Ip introducida no válida"
-        elif [ -z "$puertoMaquina" ] || [ 0 -gt $puertoMaquina ] ||
-            [ $puertoMaquina -gt 65535 ]
-        then
-            echo "Puerto introducido no válido"
         else
-            echo "$((indice * 2 - 1));$ipMaquina;$puertoMaquina-" >> servidores.conf
-            echo "$((indice * 2 ));$ipMaquina;$puertoMaquina-" >> servidores.conf
+            echo "$((indice * 2 - 1));$ipMaquina;" >> servidores.conf
+            echo "$((indice * 2 ));$ipMaquina;" >> servidores.conf
             maquinas[$indice]=$ipMaquina
-            puertos[$indice]=$puertoMaquina
             indice=$((indice + 1))
         fi
     done
@@ -43,11 +36,11 @@ function verbose {
     do
         echo "- Desplegando en $ip"
 	echo "$((indice * 2 - 1));$((indice * 2))" > servidores.locales
-        ssh $ip "rm -f -R ~/Documentos/trabajo_isis; mkdir ~/Documentos/trabajo_isis; JRE_HOME=/opt/jdk1.8.0_60; JAVA_HOME=/opt/jdk1.8.0_60; exit"
+        ssh i0917867@$ip "rm -f -R ~/Documentos/trabajo_isis; mkdir ~/Documentos/trabajo_isis; JRE_HOME=/opt/jdk1.8.0_60; JAVA_HOME=/opt/jdk1.8.0_60"
         scp tomcat.tar.gz servidores.conf servidores.locales i0917867@$ip:~/Documentos/trabajo_isis/
-        ssh $ip "cd ~/Documentos/trabajo_isis; tar -zxf ~/Documentos/trabajo_isis/tomcat.tar.gz; rm ~/Documentos/trabajo_isis/tomcat.tar.gz;"
+        ssh i0917867@$ip "cd ~/Documentos/trabajo_isis; tar -zxf ~/Documentos/trabajo_isis/tomcat.tar.gz; rm ~/Documentos/trabajo_isis/tomcat.tar.gz"
         echo "- - Arrancando servidor"
-        ssh $ip "cd ~/Documentos/trabajo_isis/TomcatServer/bin; bash startup.sh start; exit;"
+        ssh i0917867@$ip "cd ~/Documentos/trabajo_isis/TomcatServer/bin; bash catalina.sh start &"
 	indice=$((indice + 1))
     done
 
